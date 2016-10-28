40f9f2297e22a45f4de1f1a20dd777dd6fb31740
Mark Ng'ang'a <maqnganga@gmail.com>
2016-10-28 17:39:11 +0300

    Added comments

--- fibonacci.py
+++ fibonacci.py
@@ -1,8 +1,12 @@
 def print_fibonacci(finish):
+    # Loop through all numbers from 0 - finish
+    # and print the fibonacii numbers
     for count in range(0, finish):
         print fibonacci(count)
 
 
+# This function is called recursively to
+# generate the  fib numbers Might be slow
 def fibonacci(finish):
     if finish == 0:
         return 0
