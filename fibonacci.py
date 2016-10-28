659d5acbf381c575d92c4daf306da84ee8955e5d
Mark Ng'ang'a <maqnganga@gmail.com>
2016-10-28 17:48:51 +0300

    Now using a generator to genarate the sequence. Much faster.

--- fibonacci.py
+++ fibonacci.py
@@ -1,16 +1,17 @@
 def print_fibonacci(finish):
     # Loop through all numbers from 0 - finish
     # and print the fibonacii numbers
-    for count in range(0, finish):
-        print fibonacci(count)
+    for number in fibonacci(finish):
+        print number
 
 
-# This function is called recursively to
-# generate the  fib numbers Might be slow
+# Generator for fibonacci numbers
+# Faster than recursion
 def fibonacci(finish):
-    if finish == 0:
-        return 0
-    elif finish == 1:
-        return 1
-    else:
-        return fibonacci(finish - 1) + fibonacci(finish - 2)
+    old = 0
+    current = 1
+    count = 1
+    yield current
+    while (count < finish):
+        current, old, count = current + old, current, count + 1
+        yield current
