c34adca482cbb24e9c1a68f9fd4938eabd92397e
Mark Ng'ang'a <maqnganga@gmail.com>
2016-10-29 23:26:37 +0300

    Fixed: Function now prints upto x instead of first x fibonacci numbers. Added clearer comments

--- fibonacci.py
+++ fibonacci.py
@@ -1,17 +1,21 @@
 def print_fibonacci(finish):
-    # Loop through all numbers from 0 - finish
-    # and print the fibonacii numbers
-    for number in fibonacci(finish):
+    '''
+        Print fibonacii numbers returned by the generator
+        Will print all fibonacci numbers between 0 and finish
+    '''
+    for number in fibonacci_generator(finish):
         print number
 
 
-# Generator for fibonacci numbers
-# Faster than recursion
-def fibonacci(finish):
+def fibonacci_generator(finish):
+    '''
+        Generator for fibonacci numbers
+        Faster than recursion
+    '''
     old = 0
+    yield old
     current = 1
-    count = 1
     yield current
-    while (count < finish):
-        current, old, count = current + old, current, count + 1
-        yield current
+    while (current < finish):
+        yield current  # Make sure we don't return a number greater than finish by yielding before increment
+        current, old = current + old, current
